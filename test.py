ac569f6d18a34acc1edeaf62a725145be615482f
xiaoBIGfeng <95206061+xiaoBIGfeng@users.noreply.github.com>
2025-01-07 14:37:31 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,9 @@
-    File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1050, in cuda
-    return self._apply(lambda t: t.cuda(device))
-  File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/nn/modules/module.py", line 900, in _apply
-    module._apply(fn)
-  File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/nn/modules/module.py", line 900, in _apply
-    module._apply(fn)
-  File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/nn/modules/module.py", line 927, in _apply
-    param_applied = fn(param)
-  File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/nn/modules/module.py", line 1050, in <lambda>
-    return self._apply(lambda t: t.cuda(device))
-  File "/mnt/diskb/penglong/anaconda3/envs/war/lib/python3.9/site-packages/torch/cuda/__init__.py", line 319, in _lazy_init
-    torch._C._cuda_init()
-RuntimeError: The NVIDIA driver on your system is too old (found version 11020). Please update your GPU driver by downloading and installing a new version from the URL: http://www.nvidia.com/Download/index.aspx Alternatively, go to: https://pytorch.org to install a PyTorch version that has been compiled with your version of the CUDA driver.
+      running build_ext
+      error: [Errno 2] No such file or directory: '/usr/local/cuda-10.2/bin/nvcc'
+      [end of output]
+  
+  note: This error originates from a subprocess, and is likely not a problem with pip.
+  ERROR: Failed building wheel for gsplat
+  Running setup.py clean for gsplat
+Failed to build gsplat
+ERROR: ERROR: Failed to build installable wheels for some pyproject.toml based projects (gsplat)
