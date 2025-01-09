080d7e2f59d1d05f3b186d99e15f5fa3093c8ef4
xiaoBIGfeng <95206061+xiaoBIGfeng@users.noreply.github.com>
2025-01-09 15:39:58 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,192 @@
-      running build_ext
-      error: [Errno 2] No such file or directory: '/usr/local/cuda-10.2/bin/nvcc'
-      [end of output]
-  
-  note: This error originates from a subprocess, and is likely not a problem with pip.
-  ERROR: Failed building wheel for gsplat
-  Running setup.py clean for gsplat
-Failed to build gsplat
-ERROR: ERROR: Failed to build installable wheels for some pyproject.toml based projects (gsplat)
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import ops
+from basicsr.utils.registry import ARCH_REGISTRY
+
+
+# Layer Norm
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape, )
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+# SE
+class SqueezeExcitation(nn.Module):
+    def __init__(self, dim, shrinkage_rate=0.25):
+        super().__init__()
+        hidden_dim = int(dim * shrinkage_rate)
+
+        self.gate = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(dim, hidden_dim, 1, 1, 0),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, dim, 1, 1, 0),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.gate(x)
+
+
+# Channel MLP: Conv1*1 -> Conv1*1
+class ChannelMLP(nn.Module):
+    def __init__(self, dim, growth_rate=2.0):
+        super().__init__()
+        hidden_dim = int(dim * growth_rate)
+
+        self.mlp = nn.Sequential(
+            nn.Conv2d(dim, hidden_dim, 1, 1, 0),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, dim, 1, 1, 0)
+        )
+
+    def forward(self, x):
+        return self.mlp(x)
+
+
+# MBConv: Conv1*1 -> DW Conv3*3 -> [SE] -> Conv1*1
+class MBConv(nn.Module):
+    def __init__(self, dim, growth_rate=2.0):
+        super().__init__()
+        hidden_dim = int(dim * growth_rate)
+
+        self.mbconv = nn.Sequential(
+            nn.Conv2d(dim, hidden_dim, 9, 1, 9//2),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, 9 , 1, 9//2, groups=hidden_dim),
+            nn.GELU(),
+            SqueezeExcitation(hidden_dim),
+            nn.Conv2d(hidden_dim, dim, 9, 1, 9//2)
+        )
+
+    def forward(self, x):
+        return self.mbconv(x)
+
+
+# CCM
+class CCM(nn.Module):
+    def __init__(self, dim, growth_rate=2.0):
+        super().__init__()
+        hidden_dim = int(dim * growth_rate)
+
+        self.ccm = nn.Sequential(
+            nn.Conv2d(dim, hidden_dim, 9 , 1, 9//2),
+            nn.GELU(), 
+            nn.Conv2d(hidden_dim, dim, 9, 1, 9//2)
+        )
+
+    def forward(self, x):
+        return self.ccm(x)
+
+
+# SAFM
+class SAFM(nn.Module):
+    def __init__(self, dim, n_levels=4):
+        super().__init__()
+        self.n_levels = n_levels
+        chunk_dim = dim // n_levels
+
+        # Spatial Weighting
+        self.mfr = nn.ModuleList([nn.Conv2d(chunk_dim, chunk_dim, 9 , 1, 9//2, groups=chunk_dim) for i in range(self.n_levels)])
+        
+        # # Feature Aggregation
+        self.aggr = nn.Conv2d(dim, dim, 9, 1, 9//2)
+        
+        # Activation
+        self.act = nn.GELU() 
+
+    def forward(self, x):
+        h, w = x.size()[-2:]
+
+        xc = x.chunk(self.n_levels, dim=1)
+        out = []
+        for i in range(self.n_levels):
+            if i > 0:
+                p_size = (h//2**i, w//2**i)
+                s = F.adaptive_max_pool2d(xc[i], p_size)
+                s = self.mfr[i](s)
+                s = F.interpolate(s, size=(h, w), mode='nearest')
+            else:
+                s = self.mfr[i](xc[i])
+            out.append(s)
+
+        out = self.aggr(torch.cat(out, dim=1))
+        out = self.act(out) * x
+        return out
+
+class AttBlock(nn.Module):
+    def __init__(self, dim, ffn_scale=2.0):
+        super().__init__()
+
+        self.norm1 = LayerNorm(dim) 
+        self.norm2 = LayerNorm(dim) 
+
+        # Multiscale Block
+        self.safm = SAFM(dim) 
+        # Feedforward layer
+        self.ccm = CCM(dim, ffn_scale) 
+
+    def forward(self, x):
+        x = self.safm(self.norm1(x)) + x
+        x = self.ccm(self.norm2(x)) + x
+        return x
+        
+        
+@ARCH_REGISTRY.register()
+class SAFMN(nn.Module):
+    def __init__(self, dim, n_blocks=8, ffn_scale=2.0, upscaling_factor=4):
+        super().__init__()
+        self.to_feat = nn.Conv2d(3, dim, 9 , 1, 9//2)
+
+        self.feats = nn.Sequential(*[AttBlock(dim, ffn_scale) for _ in range(n_blocks)])
+
+        self.to_img = nn.Sequential(
+            nn.Conv2d(dim, 3 * upscaling_factor**2, 9 , 1, 9//2),
+            nn.PixelShuffle(upscaling_factor)
+        )
+
+    def forward(self, x):
+        x = self.to_feat(x)
+        x = self.feats(x) + x
+        x = self.to_img(x)
+        return x
+
+
+
+if __name__== '__main__':
+    #############Test Model Complexity #############
+    from fvcore.nn import flop_count_table, FlopCountAnalysis, ActivationCountAnalysis, parameter_count   
+
+    x = torch.randn(1, 3, 64, 64)
+
+
+    model = SAFMN(dim=96, n_blocks=8, ffn_scale=2.0, upscaling_factor=2)
+
+    # print(model)
+    # print(f'params: {sum(map(lambda x: x.numel(), model.parameters()))}')
+    # print(flop_count_table(FlopCountAnalysis(model, x), activations=ActivationCountAnalysis(model, x)))
+    
+
+    output = model(x)
+    print(output.shape)
+    flops = FlopCountAnalysis(model, x)
+    print("FLOPs:(G) ", flops.total()/(10**9))
+    total_params = sum(p.numel() for p in model.parameters())
+    print("Total Parameters:(M)", total_params/(10**6))
