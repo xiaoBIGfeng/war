30c76150d13ebea4637d972aa5df0b35e20d096e
xiaoBIGfeng <95206061+xiaoBIGfeng@users.noreply.github.com>
2025-01-24 16:00:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} - (?P<imgname>\S+)\s+- PSNR: (?P<psnr>[\d\.]+) dB; SSIM: (?P<ssim>[\d\.]+); PSNR_Y: (?P<psnr_y>[\d\.]+) dB; SSIM_Y: (?P<ssim_y>[\d\.]+)\.'
+if __name__ == '__main__':
+    import torch
+    B = 14  
+    H = 48  
+    W = 48  
+    frames = torch.rand(1, B, 4, H, W).cuda()
+    model = Base_Model().cuda()
+    from pytorch_lightning.utilities.model_summary import summarize
+    summary = summarize(model)
+    print(summary)
+    model.eval()
+    # image = model(frames)
+    # print('image.shape:',image.shape) 
+    # from fvcore.nn import FlopCountAnalysis, parameter_count
+    # flops = FlopCountAnalysis(model, frames)
+    # print("FLOPs:(G) ", flops.total()/(10**9))
+    total_params = sum(p.numel() for p in model.parameters())
+    print("Total Parameters:(M)", total_params/(10**6))
